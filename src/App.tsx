b2a74e0b7e28bb186a85bfa48ba8dbb75855913c
Fabio Napoleoni <f.napoleoni@gmail.com>
2020-04-25 16:14:46 +0200

    Add layout with semantic react

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, {lazy, Suspense} from "react";
 import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
-import {Loader, Dimmer} from "semantic-ui-react";
+import {Loader, Dimmer, Menu, Grid} from "semantic-ui-react";
 
 const About = lazy(() => import("./About"));
 const Home = lazy(() => import("./Home"));
@@ -8,24 +8,20 @@ const Home = lazy(() => import("./Home"));
 const App: React.FC = () => (
     <Router>
         <Suspense fallback={<Dimmer active children={<Loader content='Loading'/>}/>}>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/about">About</Link>
-                    </li>
-                </ul>
-            </nav>
-            <Switch>
-                <Route path="/about">
-                    <About/>
-                </Route>
-                <Route path="/">
-                    <Home/>
-                </Route>
-            </Switch>
+            <Menu inverted fixed={'top'}>
+                <Menu.Item as={Link} to='/' content='Home'/>
+                <Menu.Item as={Link} to='/about' content='About'/>
+            </Menu>
+            <Grid padded style={{paddingTop: '50px'}}>
+                <Switch>
+                    <Route path="/about">
+                        <About/>
+                    </Route>
+                    <Route path="/">
+                        <Home/>
+                    </Route>
+                </Switch>
+            </Grid>
         </Suspense>
     </Router>
 );
